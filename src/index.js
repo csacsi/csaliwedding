538b83175e65c3affed4ff9089710429bad9596f
csacsika <csaba@bitraptors.com>
2020-05-21 17:08:10 +0200

    lilla

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function component() {
     const element = document.createElement("div");
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(["Hello", "webpack2"], " ");
+    element.innerHTML = _.join(["Hello", "Lilla"], " ");
 
     return element;
 }
